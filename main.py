69e2d59ac5934aab13aed0cdb5e127a74d315913
R1kHardt <120347937+R1kHardt@users.noreply.github.com>
2022-12-15 00:01:55 -0500

    third class

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from dino_runner.components.game import Game
 
-if __name__ == "__main__":
+if __name__ == "_main_":
     game = Game()
-    game.run()
+    game.execute()
     print("hello there...")
--- obstacle_manager.py
+++ obstacle_manager.py
@@ -1,36 +1,43 @@
 import pygame
+import random
 
-from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS, BIRD
+from dino_runner.utils.constants import SMALL_CACTUS
 from dino_runner.components.obstacles.cactus import Cactus
-from dino_runner.components.obstacles.large_cactus import Large_Cactus
 from dino_runner.components.obstacles.bird import Bird
 
 
 class ObstacleManager:
-    def __init__(self):
-        self.obstacles = [SMALL_CACTUS,LARGE_CACTUS,BIRD]
+  def __init__(self):
+    self.obstacles = []
+    
+  def generate_obstacle(self, obstacle_type):
+    if obstacle_type == 0:
+      cactus_type = 'SMALL'
+      obstacle = Cactus(cactus_type)
+    elif obstacle_type == 1:
+      cactus_type = 'LARGE'
+      obstacle = Cactus(cactus_type)
+    else:
+      obstacle = Bird()
+    return obstacle
+    
+  def update(self, game):
+    if len(self.obstacles) == 0:
+      obstacle_type = random.randint(0, 2)
+      obstacle = self.generate_obstacle(obstacle_type)
+      self.obstacles.append(obstacle)
+      
+    for obstacle in self.obstacles:
+      obstacle.update(game.game_speed, self.obstacles)
+      if game.player.dino_rect.colliderect(obstacle.rect):
+        pygame.time.delay(1000)
+        game.death_count += 1
+        game.playing = False
+        break
+  
+  def draw(self, screen):
+    for obstacle in self.obstacles:
+      obstacle.draw(screen)
 
-    def update(self, game):
-        if len(self.obstacles) == (0, 2) == 0:
-            cactus = Cactus(SMALL_CACTUS)
-            self.obstacles.append(cactus)
-        elif len(self.obstacles) == (0, 2) == 1:
-            cactus = Large_Cactus(LARGE_CACTUS)
-            self.obstacles.append.update(cactus)  
-        elif len(self.obstacles) == (0, 2) == 2:
-            bird = Bird(BIRD)
-            self.obstacles.append(bird)    
-
-        for obstacle in self.obstacles:
-          obstacle.update(game.game_speed, self.obstacles)
-          if game.player.dino_rect.colliderect(obstacle.rect):
-            pygame.time.delay(1000)
-            game.playing = False
-            break
-
-
-
-    def draw(self,screen):
-        for obstacle in self.obstacles:
-          obstacle.draw(screen)
-          
+  def reset_onstacles(self):
+    self.obstacles = []
